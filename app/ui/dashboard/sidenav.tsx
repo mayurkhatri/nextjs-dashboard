29fe3898dfd7fa1525f0d0e4f88fb482b03bafd7
Mayur <mayurkhatri122@gmail.com>
2024-08-09 17:08:33 +0530

    Add navigation links - cleanup

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -12,11 +12,9 @@ export default function SideNav() {
       >
         <div className="w-32 text-white md:w-40">
           <AcmeLogo />
-          <div>Hi there 12</div>  
         </div>
       </Link>
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
-        <div>Hi there</div>
         <NavLinks />
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
         <form>
