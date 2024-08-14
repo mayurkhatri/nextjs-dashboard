a7c0809327a4784b63ef50cac655ba8dffeab33d
Mayur <mayurkhatri122@gmail.com>
2024-08-14 14:49:31 +0530

    Fully functional application with login functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,17 @@
-import '@/app/ui/global.css';
+import "@/app/ui/global.css";
 
-import { inter } from '@/app/ui/fonts';
- 
+import { inter } from "@/app/ui/fonts";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Acme Dashboard",
+    default: "Acme Dashboard",
+  },
+  description: "Test description",
+  metadataBase: new URL("https://nextjs-dashboard-dt96.vercel.app"),
+};
+
 export default function RootLayout({
   children,
 }: {
